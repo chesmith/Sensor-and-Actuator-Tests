a6eb094a08cc1d9113cac3a27467859b2b5bd75a
Che Smith <cwsmith@usa.net>
2019-01-15 21:43:02 -0600

    Digital input

--- Test-Sensor_DigitalInput/src/main/java/frc/robot/Robot.java
+++ Test-Sensor_DigitalInput/src/main/java/frc/robot/Robot.java
@@ -11,6 +11,8 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import edu.wpi.first.wpilibj.DigitalInput;  //!!!
+
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
@@ -24,6 +26,8 @@ public class Robot extends TimedRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
+  private DigitalInput _digitalInput; //!!!
+
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
@@ -33,6 +37,8 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+
+    _digitalInput = new DigitalInput(0);  //!!!
   }
 
   /**
@@ -86,6 +92,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
+    boolean value = _digitalInput.get();  //!!!
+    SmartDashboard.putBoolean("Value", value);  //!!!
   }
 
   /**
