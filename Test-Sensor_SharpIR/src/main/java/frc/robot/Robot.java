a3a1803fc9862df73c5581f10f3b2df9ddb2b1f0
Che Smith <cwsmith@usa.net>
2019-01-15 21:47:36 -0600

    Sharp IR rangefinder (analog)

--- Test-Sensor_SharpIR/src/main/java/frc/robot/Robot.java
+++ Test-Sensor_SharpIR/src/main/java/frc/robot/Robot.java
@@ -11,6 +11,8 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import edu.wpi.first.wpilibj.AnalogInput; //!!!
+
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
@@ -24,6 +26,8 @@ public class Robot extends TimedRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
+  private AnalogInput _sharpIR; //!!!
+
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
@@ -33,6 +37,8 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+
+    _sharpIR = new AnalogInput(0);  //!!!
   }
 
   /**
@@ -86,6 +92,18 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
+    //!!!BEGIN
+    double voltage = _sharpIR.getVoltage();
+    SmartDashboard.putNumber("Voltage", voltage);
+
+    int raw = _sharpIR.getValue();
+    SmartDashboard.putNumber("Raw", raw);
+
+    double cm = 10650.08 * Math.pow((raw / 4), -0.935) - 10;
+    double in = 4192.936 * Math.pow((raw / 4), -0.935) - 3.937;
+    SmartDashboard.putNumber("cm", cm);
+    SmartDashboard.putNumber("in", in);
+    //!!!END
   }
 
   /**
